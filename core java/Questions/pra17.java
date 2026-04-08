02a06790b78c944d01a1c3d8b29c4fb50f71f69f
Avadhut Auti <166744503+devavadhut@users.noreply.github.com>
2026-04-08 19:46:15 +0530

    ...

--- core java/Questions/pra17.java
+++
@@ -1,7 +0,0 @@
-package Questions;
-
-public class pra17 {
-    public void display() {
-        System.out.println("Hello from MyClass in mypackage");
-    }
-}
--- core java/Questions/pra171.java
+++
@@ -1,8 +0,0 @@
-package Questions;
-
-public class pra171 {
-	 public static void main(String[] args) {
-	        pra17 obj = new pra17();
-	        obj.display();
-}
-}
